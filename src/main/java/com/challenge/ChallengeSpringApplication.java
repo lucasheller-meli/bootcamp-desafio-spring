44c684141e6dfab99e17be528c5e6d0ca3ab9a49
Gabriel Tavares <gabriel.tavares@mercadolivre.com>
2021-05-31 18:21:57 -0300

    commit inicial

---
+++ src/main/java/com/challenge/ChallengeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSpringApplication.class, args);
+	}
+
+}
