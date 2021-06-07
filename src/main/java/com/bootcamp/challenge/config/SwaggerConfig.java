8eb702d1499eb0607ae34330f3131a35e9f37600
Lucas Heller <lucas.heller@mercadolivre.com>
2021-06-07 10:54:22 -0300

    Criar tags do swagger

--- src/main/java/com/bootcamp/challenge/config/SwaggerConfig.java
+++ src/main/java/com/bootcamp/challenge/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,6 +22,11 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.bootcamp.challenge.controllers"))
                 .paths(PathSelectors.any())
                 .build()
+                .tags(  new Tag("Usuarios", "Gerencia os usuários da aplicacacao."),
+                        new Tag("Produtos", "Gerencia os produtos da aplicacao"),
+                        new Tag("Publicacao", "Gerencia as publicacoes da aplicacao"),
+                        new Tag("Seguir", "Gerencia os seguidores da aplicacao")
+                )
                 .apiInfo(
                         new ApiInfoBuilder()
                                 .title("Challenge Spring")
